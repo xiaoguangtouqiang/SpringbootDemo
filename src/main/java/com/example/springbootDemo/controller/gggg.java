b8e00507bd4c7f2d74fb880ee627c39aeb5cd49d
xiaoguangtouqiang <wangdi@ch.com>
2017-06-19 22:12:34 +0800

    ddd

---
+++ src/main/java/com/example/springbootDemo/controller/gggg.java
@@ -0,0 +1,7 @@
+package com.example.springbootDemo.controller;
+
+/**
+ * Created by Administrator on 2017/6/19 0019.
+ */
+public class gggg {
+}
---
+++ src/main/java/com/example/springbootDemo/domain/eeee.java
@@ -0,0 +1,7 @@
+package com.example.springbootDemo.domain;
+
+/**
+ * Created by Administrator on 2017/6/19 0019.
+ */
+public class eeee {
+}
