cbc8b0c3a290a37050def9a461ba8dffc3ad90dd
xiaoguangtouqiang <wangdi@ch.com>
2017-06-19 22:14:10 +0800

    eeeee

--- src/main/java/com/example/springbootDemo/controller/HelloController.java
+++ src/main/java/com/example/springbootDemo/controller/HelloController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/hello")
     public String index() {
-        return "ccccddd";
+        return "eee";
     }
 }
