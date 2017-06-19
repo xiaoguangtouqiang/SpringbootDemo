2084e8a24d145131712137784bfdf6f25298f302
xiaoguangtouqiang <wangdi@ch.com>
2017-06-19 21:22:04 +0800

    1111

--- src/main/java/com/example/springbootDemo/controller/HelloController.java
+++ src/main/java/com/example/springbootDemo/controller/HelloController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/hello")
     public String index() {
-        return "Hello Test";
+        return "1111";
     }
 }
