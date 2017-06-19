5370d17ba751af39900529e713c8060b88f6f7cd
xiaoguangtouqiang <wangdi@ch.com>
2017-06-19 20:47:21 +0800

    first commit

---
+++ src/main/java/com/example/springbootDemo/SpringbootDemoApplication.java
@@ -0,0 +1,16 @@
+package com.example.springbootDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+
+@SpringBootApplication
+@Controller
+public class SpringbootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDemoApplication.class, args);
+    }
+
+
+}
---
+++ src/main/java/com/example/springbootDemo/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.springbootDemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Administrator on 2017/6/18 0018.
+ */
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String index() {
+        return "Hello World";
+    }
+}
