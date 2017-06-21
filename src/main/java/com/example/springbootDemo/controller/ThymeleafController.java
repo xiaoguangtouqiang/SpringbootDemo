2d5808559944ada591ea93b8461dc73c86479ca2
xiaoguangtouqiang <wangdi@ch.com>
2017-06-21 23:12:48 +0800

    thymeleaf

---
+++ src/main/java/com/example/springbootDemo/controller/ThymeleafController.java
@@ -0,0 +1,19 @@
+package com.example.springbootDemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Administrator on 2017/6/21 0021.
+ */
+@Controller
+public class ThymeleafController {
+    @RequestMapping("/")
+    public String index(ModelMap map) {
+        // 加入一个属性，用来在模板中读取
+        map.addAttribute("host", "http://blog.didispace.com");
+        // return模板文件的名称，对应src/main/resources/templates/index.html
+        return "index";
+    }
+}
